8ddd2f3cf19beeaeb000cc5970036c9d03366cae
igorshimauti <igorshimauti@gmail.com>
2022-07-11 21:00:33 -0300

    criados metodos recuperarPorId e recuperarTodos

--- src/main/java/br/com/projetocurso/petshop/controller/ClienteController.java
+++ src/main/java/br/com/projetocurso/petshop/controller/ClienteController.java
@@ -5,12 +5,10 @@ import br.com.projetocurso.petshop.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "cliente")
@@ -24,5 +22,22 @@ public class ClienteController {
         Cliente clienteIncluido = clienteService.salvar(cliente);
         URI localizacao = URI.create("cliente/" + clienteIncluido.getId());
         return ResponseEntity.created(localizacao).body(clienteIncluido);
+    }
+
+    @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Cliente> atualizar(@PathVariable Long id, @RequestBody Cliente cliente) {
+        cliente.setId(id);
+        Cliente clienteAtualizado = clienteService.salvar(cliente);
+        return ResponseEntity.ok().body(clienteAtualizado);
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Cliente> recuperarPorId(@PathVariable Long id) {
+        return ResponseEntity.ok().body(clienteService.recuperarPorId(id));
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<Cliente>> recuperarTodos() {
+        return ResponseEntity.ok().body(clienteService.recuperarTodos());
     }
 }
--- src/main/java/br/com/projetocurso/petshop/service/ClienteService.java
+++ src/main/java/br/com/projetocurso/petshop/service/ClienteService.java
@@ -5,6 +5,8 @@ import br.com.projetocurso.petshop.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ClienteService {
 
@@ -14,5 +16,13 @@ public class ClienteService {
     public Cliente salvar(Cliente cliente) {
         Cliente clienteSalvo = clienteRepository.save(cliente);
         return clienteSalvo;
+    }
+
+    public Cliente recuperarPorId(Long id) {
+        return clienteRepository.findById(id).orElse(null);
+    }
+
+    public List<Cliente> recuperarTodos() {
+        return clienteRepository.findAll();
     }
 }
